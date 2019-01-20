4d9e13e52d47cfeb1164f7f963578aa9aac4bccc
Arpad Belovai <rp@belovai.com>
2019-01-20 14:22:13 +0100

    Add fields

--- goipapi.go
+++ goipapi.go
@@ -10,15 +10,19 @@ const apiURL = "http://ip-api.com/"
 
 type Client struct {
 	Format string
+	Fields string
 }
 
 func NewClient(format string) *Client {
-	return &Client{Format: format}
+	return &Client{
+		Format: format,
+		Fields: "status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,reverse,mobile,proxy,query",
+	}
 }
 
 //LookupIP some ....
 func (c *Client) LookupIP(ip string) (jsonString string, err error) {
-	url := fmt.Sprintf("%s/%s/%s", apiURL, c.Format, ip)
+	url := fmt.Sprintf("%s/%s/%s?fields=%s", apiURL, c.Format, ip, c.Fields)
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -39,4 +43,9 @@ func (c *Client) LookupIP(ip string) (jsonString string, err error) {
 	jsonString = buff.String()
 
 	return
+}
+
+func (c *Client) SetFields(fields string) *Client {
+	c.Fields = fields
+	return c
 }
